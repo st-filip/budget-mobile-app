e1d0de83df466c9b643b8f4e44785101120f6859
st-filip <fstamenkovic26@gmail.com>
2024-05-02 15:51:26 +0200

    Obrisani komentari

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,18 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: HomePage,
-  // },
-  // {
-  //   path: 'envelopes',
-  //   loadChildren: () => import('./envelopes/envelopes.module').then( m => m.EnvelopesPageModule)
-  // },
-  // {
-  //   path: 'transactions',
-  //   loadChildren: () => import('./transactions/transactions.module').then( m => m.TransactionsPageModule)
-  // }
   {
     path: 'tabs',
     component: HomePage,
