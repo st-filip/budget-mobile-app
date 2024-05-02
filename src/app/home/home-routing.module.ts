ad040977d61569e611707586af4f72db8c8fc652
st-filip <fstamenkovic26@gmail.com>
2024-05-02 15:49:53 +0200

    Dodat Tabs navigacioni meni i podešene rute

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,22 +3,52 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomePage,
+  // },
+  // {
+  //   path: 'envelopes',
+  //   loadChildren: () => import('./envelopes/envelopes.module').then( m => m.EnvelopesPageModule)
+  // },
+  // {
+  //   path: 'transactions',
+  //   loadChildren: () => import('./transactions/transactions.module').then( m => m.TransactionsPageModule)
+  // }
+  {
+    path: 'tabs',
+    component: HomePage,
+    children: [
+      {
+        path: 'envelopes',
+        loadChildren: () =>
+          import('./envelopes/envelopes.module').then(
+            (m) => m.EnvelopesPageModule
+          ),
+      },
+      {
+        path: 'transactions',
+        loadChildren: () =>
+          import('./transactions/transactions.module').then(
+            (m) => m.TransactionsPageModule
+          ),
+      },
+      {
+        path: '',
+        redirectTo: '/home/tabs/envelopes',
+        pathMatch: 'full',
+      },
+    ],
+  },
   {
     path: '',
-    component: HomePage,
+    redirectTo: '/home/tabs/envelopes',
+    pathMatch: 'full',
   },
-  {
-    path: 'envelopes',
-    loadChildren: () => import('./envelopes/envelopes.module').then( m => m.EnvelopesPageModule)
-  },
-  {
-    path: 'transactions',
-    loadChildren: () => import('./transactions/transactions.module').then( m => m.TransactionsPageModule)
-  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class HomePageRoutingModule {}
